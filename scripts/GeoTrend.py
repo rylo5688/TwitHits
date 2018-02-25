8d4a5a5903641e69605dd524b3a08373ff80c7ce
loi.ryan18@gmail.com <loi.ryan18@gmail.com>
2018-02-24 20:56:07 -0700

    progress on nearbyTrends and searchTweet

--- scripts/GeoTrend.py
+++ scripts/GeoTrend.py
@@ -9,21 +9,47 @@ api = tweepy.API(auth)
 def hexParse(tweet):
     # Change from hard code later... Need unicode conversion
     tweet = tweet.replace("%23", "#")
-    tweet = tweet.replace("%22", "\"")
+    tweet = tweet.replace("%22", "")
     tweet = tweet.replace("+", " ")
     return tweet
 
 def nearbyTrends(latitude, longitude):
     geoID = api.trends_closest(latitude, longitude)
-
+    print geoID[0]['woeid']
     trending_tweets = api.trends_place(geoID[0]['woeid'])
     print "TOTAL TWEETS: " + str(len(trending_tweets[0]['trends']))
 
     trends = []
+    count = 0
     for tweet in trending_tweets[0]['trends']:
+        if (count >= 10):
+            break
+
         trends.append(hexParse(str(tweet['query'])))
+        count = count+1
+
+    searchTweet(trends, latitude, longitude, 8)
 
     return trends
 
+def searchTweet(trends, latitude, longitude, radius):
+    # API.search(q[, lang][, locale][, rpp][, page][, since_id][, geocode][, show_user])
+    gCode = str(latitude) + "," + str(longitude) + "," + str(radius) + "km"
+
+    queryAndTweet = {}
+    for query in trends:
+        results = api.search(q=query, count=1, geocode=gCode)
+
+        if query in queryAndTweet:
+            queryAndTweet[query].append([results])
+        else:
+            queryAndTweet[query] = [results]
+
+        # if len(results) > 0:
+            # queryAndTweet.append({query : [] })
+
+
+
+
 if __name__ == "__main__":
-    print nearbyTrends(27.6648, 81.5158)
+    print nearbyTrends(40.0068, -105.2628)
