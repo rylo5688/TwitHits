5cbc6eff2fd647a9fbb22342d12f84d42b844784
Eric Kim <erickim@Erics-MacBook-Pro.local>
2018-02-24 19:27:55 -0700

    added early version of my part.

---
+++ scripts/locationTweets.py
@@ -0,0 +1,25 @@
+from access import Access
+import tweepy
+######## AUTHENTICATION
+auth = tweepy.OAuthHandler(Access.consumer_key, Access.consumer_secret)
+auth.set_access_token(Access.access_token, Access.access_token_secret)
+
+api = tweepy.API(auth)
+########
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print tweet.text
+
+#### Embeded Tweets
+#area = api.search("Tom Hanks", lang='en', rpp=5, page=1, geocode="35.9078,127.7669,150km", show_user=True)
+
+for tweet in tweepy.Cursor(api.search,
+                           q="Twice",
+                           rpp=100,
+                           geocode="35.9078,127.7669,150km",
+                           result_type="recent",
+                           include_entities=True,
+                           lang="en",
+                           show_user=True).items():
+    print(tweet.text)
+  
